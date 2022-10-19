fc20c9e5ed48c812a56d52849170475327630fd6
Mahesh <gmahesh007babu@gmail.com>
2022-10-20 00:50:52 +0530

    registration added to hit the api

--- pages/register.vue
+++ pages/register.vue
@@ -1,11 +1,37 @@
 <template>
-  <form>
+  <form @submit.prevent="submit">
     <h1 class="h3 mb-3 fw-normal">
       Please Register
     </h1>
-    <input type="text" class="form-control" placeholder="Name" required></input>
-    <input type="email" class="form-control" placeholder="Email" required>
-    <input type="password" class="form-control" placeholder="Password" required>
+    <input v-model="username" type="text" class="form-control" placeholder="Name" required></input>
+    <input v-model="email" type="email" class="form-control" placeholder="Email" required>
+    <input v-model="password" type="password" class="form-control" placeholder="Password" required>
     <button class=" btn btn-lg btn-primary" type="submit">Sign Up</button>
   </form>
 </template>
+<script>
+  export default {
+    name: "register",
+    data(){
+      return {
+        username: '',
+        email: '',
+        password: ' '
+      }
+    },
+    methods: {
+      async submit(){
+        await fetch('http://localhost:3000/users', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            user_name: this.username,
+            email: this.email,
+            password: this.password
+          })
+        } );
+        await this.$router.push('/login');
+      }
+    }
+  }
+</script>
