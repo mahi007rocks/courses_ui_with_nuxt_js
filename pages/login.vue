bd6444540cec47b1c86fe2edab5981bbbfb3eb95
Mahesh <gmahesh007babu@gmail.com>
2022-10-20 10:17:46 +0530

    Login page added

--- pages/login.vue
+++ pages/login.vue
@@ -1,10 +1,34 @@
 <template>
-  <form>
+  <form @submit.prevent="submit">
     <h1 class="h3 mb-3 fw-normal">
       Please sign in
     </h1>
-    <input type="email" class="form-control" placeholder="Email" required>
-    <input type="password" class="form-control" placeholder="Password" required>
+    <input v-model="email" type="email" class="form-control" placeholder="Email" required>
+    <input v-model="password" type="password" class="form-control" placeholder="Password" required>
     <button class=" btn btn-lg btn-primary" type="submit">Sign in</button>
   </form>
 </template>
+<script>
+  export default  {
+    name: 'login',
+    data() {
+      return {
+        email: '',
+        password: ''
+      }
+    },
+      methods: {
+        async submit(){
+          await fetch('http://localhost:3000/auth/login', {
+          method: 'POST',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            email: this.email,
+            password: this.password
+          })
+        } );
+        await this.$router.push('/');
+      }
+    }
+  }
+</script>
